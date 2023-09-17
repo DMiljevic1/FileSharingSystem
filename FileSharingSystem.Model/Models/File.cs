2aa80b97bc7cb1c9558684afbcb3b0b323482629
unknown <duje.miljevic@gmail.com>
2023-09-17 10:14:25 +0200

    added domain models

---
+++ FileSharingSystem.Model/Models/File.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSharingSystem.Model.Models
+{
+	public class File
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int FolderId { get; set; }
+		public Folder? Folder { get; set; }
+		public long Size { get; set; }
+		public byte[] Attachment { get; set; }
+		public string Extension { get; set; }
+	}
+}
