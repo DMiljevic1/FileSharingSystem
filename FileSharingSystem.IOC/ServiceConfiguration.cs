ca57f7e46db97ead646152ecef733667d7631440
unknown <duje.miljevic@gmail.com>
2023-09-20 20:12:17 +0200

    added connection string and IOC project

---
+++ FileSharingSystem.IOC/ServiceConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using FileSharingSystem.Repository.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSharingSystem.IOC
+{
+	public static class ServiceConfiguration
+	{
+		public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+		{
+			ConfigureRepositories(services, configuration);
+		}
+
+		private static void ConfigureRepositories(IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddDbContext<FileSharingSystemDbContext>(options => options.UseSqlServer(
+			configuration.GetConnectionString("DefaultConnection")
+			));
+			// dodat ode sve scopove za repositorie
+		}
+	}
+}
---
+++ FileSharingSystem.Repository/DatabaseContext/FileSharingSystemDbContext.cs
@@ -0,0 +1,22 @@
+﻿using FileSharingSystem.Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSharingSystem.Repository.DatabaseContext
+{
+	public class FileSharingSystemDbContext : DbContext
+	{
+		public FileSharingSystemDbContext(DbContextOptions<FileSharingSystemDbContext> dbContextOptions) : base(dbContextOptions) { }
+
+		public DbSet<User> Users {  get; set; }
+		public DbSet<Group> Groups { get; set; }
+		public DbSet<Folder> Folders { get; set; }
+		public DbSet<Role> Roles { get; set; }
+		public DbSet<Model.Models.File> Files { get; set; }
+		public DbSet<UserGroupRelation> UserGroupRelations { get; set; }
+	}
+}
